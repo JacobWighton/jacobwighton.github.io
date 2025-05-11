a62cfb63e8e93df5ad9ae46f7e4a25325699113b
JacobWighton <73089978+JacobWighton@users.noreply.github.com>
2025-05-11 19:34:44 +1000

    Replaced test site with new site

--- test-site/scripts/tnfSearchTermScript.js
+++
@@ -1,5 +0,0 @@
-const pageTitle = document.querySelector(".page-title");
-const pattern = /(?<=\bq=)([\w\+]+)/;
-const currentUrl = window.location.href;
-const searchTerm = pattern.exec(currentUrl)[0];
-pageTitle.textContent = "Search: " + searchTerm;
